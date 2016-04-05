c002375a6bb184690b4687beba5d48cdc111a2cd
David Brown <david.brown@pnnl.gov>
2016-04-05 15:11:38 -0700

    version bump to reflect the omnibus update

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'david.brown@pnnl.gov'
 license 'all_rights'
 description 'Installs/Configures ecology-cluster'
 long_description 'Installs/Configures ecology-cluster'
-version '0.1.0'
+version '0.1.1'
 
 depends 'chef-client'
 depends 'omnibus_updater'
